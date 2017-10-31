64cb3873161ec77dfd0da85ca86e9e2c54a4a8f9
Wangenga_Mwai <wangenga.peter@gmail.com>
2017-10-31 16:46:30 +0300

    initial structure for the frontend app

---
+++ src/config/router.js
@@ -0,0 +1,27 @@
+import Home from '../spa/Home'
+import Login from '../spa/login/Login'
+import NotFound from '../spa/NotFound'
+/* eslint-disable import/prefer-default-export */
+export const routes = [
+  {
+    path: '/',
+    component: Home,
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '*',
+    component: NotFound
+  }
+]
+
+export const VueRouterObject = {
+  routes,
+  mode: 'history',
+  saveScrollPosition: true
+}
+
+export default VueRouter => new VueRouter(VueRouterObject)
